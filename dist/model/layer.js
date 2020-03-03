bf8b67053bf2d9a3d5c0d2718d7034d5e8551fec
azran4u <azran4u@gmail.com>
2020-03-03 22:41:57 +0200

    applied gitignore

--- dist/model/layer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LAYER;
-(function (LAYER) {
-    LAYER[LAYER["BASE_LAYER"] = 0] = "BASE_LAYER";
-    LAYER[LAYER["EL1"] = 1] = "EL1";
-    LAYER[LAYER["EL2"] = 2] = "EL2";
-})(LAYER = exports.LAYER || (exports.LAYER = {}));
-//# sourceMappingURL=layer.js.map
--- dist/model/simulation/simulationResults.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SimulationResults {
-}
-exports.SimulationResults = SimulationResults;
-//# sourceMappingURL=simulationResults.js.map
